c2e360ad4858510800c78be8b36681fe4109d5e1
Jeremy-NZ <jeremydrummond11@gmail.com>
2022-12-28 11:21:09 +1300

    hack banner local storage

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+localStorage.setItem('bannerLastClosed', new Date()); 
+localStorage.setItem('xpromo-consolidation', new Date()); 
+
 setTimeout(function () {
   console.log('reddlimit started');
   
-  var elementsToRemove = document.querySelectorAll(".XPromoPopupRpl, .OuterBannerContainer,.XPromoBottomBar,.PaginationButtons");
+  var elementsToRemove = document.querySelectorAll(".OuterBannerContainer,.XPromoBottomBar,.PaginationButtons");
   for (var i =0; i<elementsToRemove.length; i++){
     elementsToRemove[i].remove();
   }
